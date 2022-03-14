9e371970e27ceed557c050be15de13b8638cecac
Georgi Botsihhin <botsihhingeorge@icloud.com>
2022-03-14 10:21:58 +0000

    Added CommitFile model

---
+++ UpdaterLibrary/Models/CommitFile.cs
@@ -0,0 +1,11 @@
+﻿namespace UpdaterLibrary.Models
+{
+    public class CommitFile
+    {
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public string Status { get; set; }
+    }
+}
